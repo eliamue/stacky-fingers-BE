e4e53aea6c5c6077edd7aba7f4063752fb73d3af
Elise Muellerleile <eliamue@live.com>
2021-09-22 23:07:48 -0500

    testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Resources from '../lib/models/Resources.js';
-
+//testing
 describe('Resources CRUD routes', () => {
   beforeEach(() => {
     return setup(pool);
