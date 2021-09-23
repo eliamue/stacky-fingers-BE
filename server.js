2ae88e6ede796899b0ef72f6ac871bed98259fa7
Elise Muellerleile <eliamue@live.com>
2021-09-22 23:17:55 -0500

    access control allow origin added to header

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ import pool from './lib/utils/pool.js';
 
 const PORT = process.env.PORT || 7890;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
