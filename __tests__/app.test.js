d73671454a3f4aaa32ff8a8b236b00a355b88f5d
Elise Muellerleile <eliamue@live.com>
2022-02-18 22:15:47 -0600

    init

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,20 +13,27 @@ describe('Resources CRUD routes', () => {
     pool.end();
   });
   it('creates a new resource', async () => {
-    const resource = {
-      src_name: 'Day One Crime Victim Support Line',
-      category: 'Crisis',
-      src_description:
-        'Day One hosts the statewide support line for general crime victims. Help is available to you no matter where you’re located in the state of Minnesota. 24/7 support line.',
-      st_address: '1000 E 80th St',
-      city: 'Bloomington',
-      zip: 55420,
-      us_state: 'MN',
-      phone: '1-(866)-223-1111',
-      text_num: '612-399-9995',
-      website: 'https://dayoneservices.org/',
-      email: 'safety@dayoneservices.org',
-      is_24_7: true,
+    const resource = 
+    {
+      title: 'Dark Reader',
+      category: 'Accessibility',
+      about:
+      'Eye-care browser extension to switch any website into dark mode with full customizability.',
+      website: 'https://darkreader.org/',
+      logo: 'https://repository-images.githubusercontent.com/26682105/4d22e080-070e-11eb-8930-c69a17260e45',
+      located: '',
+      tags: [
+        'photophobia',
+        'eye-care',
+        'dark mode',
+        'computer filter',
+        'light sensitivity',
+        'vision',
+        'technology',
+        'open-source',
+        'free',
+        'sensory processing',
+      ],
     };
 
     const res = await request(app).post('/api/v1/resources').send(resource);
@@ -39,84 +46,81 @@ describe('Resources CRUD routes', () => {
 
   it('gets all resources', async () => {
     const resource1 = await Resources.createResource({
-      src_name: 'Tubman Family Crisis and Support Services',
-      category: 'Mental Health',
-      src_description:
-        'Tubman helps people of all ages, genders, and cultural backgrounds who have experienced relationship violence, elder abuse, addiction, sexual exploitation or other forms of trauma. Throughout the Twin Cities, Tubman provides safe shelter, legal services, mental and chemical health counseling, youth programming, and community education, including public information campaigns to provide community members the information and support they need to get help or give help.',
-      st_address: '4432 Chicago Avenue South',
-      city: 'Minneapolis',
-      zip: 55407,
-      us_state: 'MN',
-      phone: '612-825-0000',
-      text_num: '612-825-3333',
-      website: 'https://www.tubman.org/',
-      email: '',
-      is_24_7: true,
+      title: 'Dark Reader',
+      category: 'Accessibility',
+      about:
+      'Eye-care browser extension to switch any website into dark mode with full customizability.',
+      website: 'https://darkreader.org/',
+      logo: 'https://repository-images.githubusercontent.com/26682105/4d22e080-070e-11eb-8930-c69a17260e45',
+      located: '',
+      tags: [
+        'photophobia',
+        'eye-care',
+        'dark mode',
+        'computer filter',
+        'light sensitivity',
+        'vision',
+        'technology',
+        'open-source',
+        'free',
+        'sensory processing',
+      ],
     });
 
-    const resource2 = await Resources.createResource({
-      src_name: 'Domestic Violence Resource Center',
-      category: 'Domestic',
-      src_description:
-        'The Domestic Violence Resource Center provides free, confidential, bilingual services in an effort to bring an end to family violence in Washoe County.',
-      st_address: '1735 Vassar Street',
-      city: 'Reno',
-      zip: 89502,
-      us_state: 'NV',
-      phone: '775-329-4150',
-      text_num: '',
-      website: 'https://domesticviolenceresourcecenter.org/',
-      email: 'info@domesticviolenceresourcecenter.org',
-      is_24_7: true,
+    const resource2 = await Resources.createResource({ title: 'f.lux',
+      category: 'Accessibility',
+      about: 'Eye-care software that adapts the color of your computer display.',
+      website: 'https://justgetflux.com/',
+      logo: 'https://tamezatu.com/wp-content/uploads/2015/05/flux.png',
+      located: '',
+      tags: [
+        'photophobia',
+        'eye-care',
+        'blue-light filter',
+        'adaptive display',
+        'computer filter',
+        'light sensitivity',
+        'vision',
+        'technology',
+        'desktop application',
+        'free',
+        'sensory processing',
+        'tinted'
+      ],
     });
 
     const resource3 = await Resources.createResource({
-      src_name: 'Multnomah County Animal Services',
-      category: 'Animals',
-      src_description:
-        'Multnomah County Animal Services is the county’s sole public animal shelter and control agency. The mission of MCAS is to protect the health, safety and welfare of people and pets throughout Multnomah County. MCAS cares for the community’s lost, homeless, stray, injured, sick, neglected and abandoned animals.',
-      st_address: '1700 W Historic River Hwy',
-      city: 'Troutdale',
-      zip: 97060,
-      us_state: 'OR',
-      phone: '503-988-7387',
-      text_num: '',
-      website: 'https://www.multcopets.org/',
-      email: '',
-      is_24_7: true,
+      title: 'Brain Trauma Foundation',
+      category: 'Education',
+      about:
+      'Develops evidence-based guidelines, research partnerships, & educational outreach.',
+      website: 'http://www.braintrauma.org/',
+      logo: 'https://med.stanford.edu/braincenter/research/_jcr_content/main/panel_builder/panel_1/panel_builder/panel_0/feature_box.img.620.high.png/neurosurgery_concussion_BTF.png',
+      located: '',
+      tags: ['peer-reviewed', 'articles', 'scholarly', 'study', 'teaching'],
     });
 
     const resource4 = await Resources.createResource({
-      src_name: 'St Paul Animal Control Center',
-      category: 'Animals',
-      src_description: '',
-      st_address: '1285 Jessamine Avenue West',
-      city: 'St Paul',
-      zip: 55108,
-      us_state: 'MN',
-      phone: '651-266-1100',
-      text_num: '651-266-8989',
-      website:
-        'https://www.stpaul.gov/departments/safety-inspections/animal-control-information',
-      email: '',
-      is_24_7: false,
+      title: 'Hennepin Health Traumatic Brain Injury Center',
+      category: 'Services',
+      about:
+      'Prevention, emergency care, neurosurgery, critical care, inpatient, & outpatient care.',
+      website: 'https://www.hennepinhealthcare.org/specialty/traumatic-brain-injury-center/',
+      logo: 'https://hereforlife.blog/wp-content/uploads/2018/03/featured-hennepin-healthcare-promo.jpg',
+      located: '*MN only',
+      tags: [
+        'Minnesota',
+        'ER',
+        'case management',
+        'medical doctor',
+        'rehabilitation',
+        'treatment',
+        'recovery',
+        'therapy',
+        'physical rehab'
+      ],
     });
 
-    const resource5 = await Resources.createResource({
-      src_name: 'Day One Crime Victim Support Line',
-      category: 'Crisis',
-      src_description:
-        'Day One hosts the statewide support line for general crime victims. Help is available to you no matter where you’re located in the state of Minnesota. 24/7 support line.',
-      st_address: '1000 E 80th St',
-      city: 'Bloomington',
-      zip: 55420,
-      us_state: 'MN',
-      phone: '1-(866)-223-1111',
-      text_num: '612-399-9995',
-      website: 'https://dayoneservices.org/',
-      email: 'safety@dayoneservices.org',
-      is_24_7: true,
-    });
 
     const res = await request(app).get('/api/v1/resources');
 
@@ -124,26 +128,31 @@ describe('Resources CRUD routes', () => {
       resource1,
       resource2,
       resource3,
-      resource4,
-      resource5,
+      resource4
     ]);
   });
 
   it('gets one resource by id', async () => {
-    const resource = await Resources.createResource({
-      src_name: 'Day One Crime Victim Support Line',
-      category: 'Crisis',
-      src_description:
-        'Day One hosts the statewide support line for general crime victims. Help is available to you no matter where you’re located in the state of Minnesota. 24/7 support line.',
-      st_address: '1000 E 80th St',
-      city: 'Bloomington',
-      zip: 55420,
-      us_state: 'MN',
-      phone: '1-(866)-223-1111',
-      text_num: '612-399-9995',
-      website: 'https://dayoneservices.org/',
-      email: 'safety@dayoneservices.org',
-      is_24_7: true,
+    const resource = await Resources.createResource({ title: 'f.lux',
+      category: 'Accessibility',
+      about: 'Eye-care software that adapts the color of your computer display.',
+      website: 'https://justgetflux.com/',
+      logo: 'https://tamezatu.com/wp-content/uploads/2015/05/flux.png',
+      located: '',
+      tags: [
+        'photophobia',
+        'eye-care',
+        'blue-light filter',
+        'adaptive display',
+        'computer filter',
+        'light sensitivity',
+        'vision',
+        'technology',
+        'desktop application',
+        'free',
+        'sensory processing',
+        'tinted'
+      ],
     });
 
     const res = await request(app).get(`/api/v1/resources/${resource.id}`);
@@ -153,65 +162,66 @@ describe('Resources CRUD routes', () => {
 
   it('updates a resource', async () => {
     const resource = await Resources.createResource({
-      src_name: 'Day One Crime Victim Support Line',
-      category: 'Crisis',
-      src_description:
-        'Day One hosts the statewide support line for general crime victims. Help is available to you no matter where you’re located in the state of Minnesota. 24/7 support line.',
-      st_address: '1000 E 80th St',
-      city: 'Bloomington',
-      zip: 55420,
-      us_state: 'MN',
-      phone: '1-(866)-223-1111',
-      text_num: '612-399-9995',
-      website: 'https://dayoneservices.org/',
-      email: 'safety@dayoneservices.org',
-      is_24_7: true,
+      title: 'Brain Injury Alliance',
+      category: 'Advocacy',
+      about:
+      'Resource facilitation, education, outreach, events, & case management, plus state-specific chapters.',
+      website: 'https://usbia.org/',
+      logo: 'https://biacolorado.org/wp-content/uploads/2011/12/USBIA-Logo.jpg',
+      located: '',
+      tags: [
+        'newsletter',
+        'case manager',
+        'support group',
+        'check-in',
+        'referrals',
+        'support services',
+      ],
     });
 
     const res = await request(app)
       .put(`/api/v1/resources/${resource.id}`)
       .send({
-        src_name: 'Day One Crime Victim Support Line',
-        category: 'Crisis',
-        src_description:
-          'Day One hosts the statewide support line for general crime victims. Help is available to you no matter where you’re located in the state of Minnesota. 24/7 support line.',
-        st_address: '1000 E 80th St',
-        city: 'Bloomington',
-        zip: 55420,
-        us_state: 'MN',
-        phone: '866-223-1111',
-        text_num: '612-399-9995',
-        website: 'https://dayoneservices.org/',
-        email: 'safety@dayoneservices.org',
-        is_24_7: true,
+        title: 'MN Brain Injury Alliance',
+        website: 'https://www.braininjurymn.org/',
+        logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQarrQg3SfqDlfnSQfliaolc8bbkjxJtH063GVjDESB4mCisUx9JdYPosoazFrR0JjYCV4&usqp=CAU',
+        located: '*MN only',
       });
-    expect(res.body).toEqual({
-      ...resource,
-      phone: '866-223-1111',
+    expect(res.body).toEqual({ ...resource,
+      title: 'MN Brain Injury Alliance',
+      website: 'https://www.braininjurymn.org/',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQarrQg3SfqDlfnSQfliaolc8bbkjxJtH063GVjDESB4mCisUx9JdYPosoazFrR0JjYCV4&usqp=CAU',
+      located: '*MN only',
     });
   });
 
   it('deletes a specific existing resource', async () => {
-    const resource = await Resources.createResource({
-      src_name: 'St Paul Animal Control Center',
-      category: 'Animals',
-      src_description: '',
-      st_address: '1285 Jessamine Avenue West',
-      city: 'St Paul',
-      zip: 55108,
-      us_state: 'MN',
-      phone: '651-266-1100',
-      text_num: '651-266-8989',
-      website:
-        'https://www.stpaul.gov/departments/safety-inspections/animal-control-information',
-      email: '',
-      is_24_7: false,
+    const resource = await Resources.createResource({ title: 'f.lux',
+      category: 'Accessibility',
+      about: 'Eye-care software that adapts the color of your computer display.',
+      website: 'https://justgetflux.com/',
+      logo: 'https://tamezatu.com/wp-content/uploads/2015/05/flux.png',
+      located: '',
+      tags: [
+        'photophobia',
+        'eye-care',
+        'blue-light filter',
+        'adaptive display',
+        'computer filter',
+        'light sensitivity',
+        'vision',
+        'technology',
+        'desktop application',
+        'free',
+        'sensory processing',
+        'tinted'
+      ],
     });
 
     const res = await request(app).delete(`/api/v1/resources/${resource.id}`);
 
     expect(res.body).toEqual({
-      message: `You have deleted ${resource.src_name}.`,
+      message: `You have deleted ${resource.title}.`,
     });
   });
 });
